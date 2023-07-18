e5acdad952d21e81de774e5da676992cefa6a336
Valera <>
2023-07-18 22:33:36 +0300

    Logging was added

---
+++ Sources/Tuvi.Core.Logging/LoggingExtension.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Tuvi.Core.Logging
+{
+    public static class LoggingExtension
+    {
+        public static ILoggerFactory _loggerFactory;
+
+        public static ILoggerFactory LoggerFactory
+        {
+            get
+            {
+                if (_loggerFactory is null)
+                {
+                    _loggerFactory = new LoggerFactory();
+                }
+                return _loggerFactory;
+            }
+            set { _loggerFactory = value; }
+        }
+        static class LoggerContainer<T>
+        {
+            internal static readonly ILogger Logger = LoggerFactory.CreateLogger<T>();
+        }
+
+        public static ILogger Log<T>() => LoggerContainer<T>.Logger;
+        public static ILogger Log<T>(this T t) => LoggerContainer<T>.Logger;
+    }
+}
