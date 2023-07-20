1435c30e182d57994fba60ed96a53d2320941d10
Valera <>
2023-07-20 15:01:37 +0300

    Dec was added

---
+++ Sources/Tuvi.Core.Dec/DecStorageClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Tuvi.Core.Dec
+{
+    public interface IDecStorageClient : IDisposable
+    {
+        Task<string> SendAsync(string address, byte[] data);
+
+        Task<IEnumerable<string>> ListAsync(string address);
+
+        Task<byte[]> GetAsync(string address, string hash);
+    }
+}
