0b20c0a489721b04f4049aa7a10f5ae735cf2b3b
Valera <50830352+ValeraFinebits@users.noreply.github.com>
2023-10-09 10:57:34 +0300

    PGP Backup identity moved (#18)
    Co-authored-by: Valera <>

--- Sources/Tuvi.Core/ImplementationDetailsProvider.cs
+++ Sources/Tuvi.Core/ImplementationDetailsProvider.cs
@@ -5,22 +5,19 @@ namespace Tuvi.Core
 {
     public class ImplementationDetailsProvider : IBackupDetailsProvider, IKeyDerivationDetailsProvider
     {
-        // TODO: Needs to be moved to the client side
-        public static string BackupPgpKeyIdentity => "backup@system.service.tuvi.com";
-
         private string _keyDerivationSalt;
         private string _backupPackageIdentifier;
         
         private Dictionary<SpecialPgpKeyType, string> _specialPgpKeyIdentities;
 
-        public ImplementationDetailsProvider(string keyDerivationSalt, string backupPackageIdentifier)
+        public ImplementationDetailsProvider(string keyDerivationSalt, string backupPackageIdentifier, string backupPgpKeyIdentity)
         {
             _keyDerivationSalt = keyDerivationSalt;
             _backupPackageIdentifier = backupPackageIdentifier;
 
             _specialPgpKeyIdentities = new Dictionary<SpecialPgpKeyType, string>
                                       {
-                                          { SpecialPgpKeyType.Backup, BackupPgpKeyIdentity }
+                                          { SpecialPgpKeyType.Backup, backupPgpKeyIdentity }
                                       };
         }        
 
