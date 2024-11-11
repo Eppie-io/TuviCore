a2fb0f729b823ba2488e6b99ed2176ced1b90544
Valera <valera@finebits.com>
2024-11-11 16:35:22 +0200

    test: Add tests

---
+++ Sources/Tests/Tuvi.Core.Backup.Impl.Tests/BackupSerializationTests.cs
@@ -0,0 +1,186 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Tuvi.Core.Backup;
+using Tuvi.Core.Backup.Impl;
+using Tuvi.Core.Entities.Exceptions;
+
+namespace BackupTests
+{
+    public class BackupSerializationTests : BaseBackupTest
+    {
+        [OneTimeSetUp]
+        protected void InitializeContext()
+        {
+            Initialize();
+        }
+
+        [Test]
+        [Category("Serialization")]
+        [Category("Backup")]
+        public async Task IntegerBinarySerializationAsync()
+        {
+            await Task.Run(() =>
+            {
+                foreach (var pair in TestData.IntegerToBufferPairs)
+                {
+                    var integer = pair.Item1;
+                    var referenceBuffer = pair.Item2;
+                    var actualBuffer = integer.ToByteBuffer();
+
+                    Assert.AreEqual(referenceBuffer, actualBuffer);
+                }
+            }).ConfigureAwait(true);
+        }
+
+        [Test]
+        [Category("Serialization")]
+        [Category("Backup")]
+        public async Task IntegerBinaryDeserializationAsync()
+        {
+            await Task.Run(() =>
+            {
+                foreach (var pair in TestData.IntegerToBufferPairs)
+                {
+                    var referenceInteger = pair.Item1;
+                    var buffer = pair.Item2;
+                    var actualInterger = buffer.FromByteBuffer();
+
+                    Assert.AreEqual(referenceInteger, actualInterger);
+                }
+            }).ConfigureAwait(true);
+        }
+
+        [Test]
+        [Category("Protection")]
+        [Category("Backup")]
+        public async Task DataProtectorTest()
+        {
+            using (var protectedData = new MemoryStream())
+            using (var deatachedSignatureData = new MemoryStream())
+            using (var publicKeyData = new MemoryStream())
+            {
+                using (var unprotectedData = new MemoryStream())
+                {
+                    await unprotectedData.WriteAsync(TestData.DataToProtect).ConfigureAwait(false);
+
+                    unprotectedData.Position = 0;
+                    await BackupDataProtector.LockDataAsync(unprotectedData, protectedData).ConfigureAwait(false);
+                    await BackupDataProtector.CreateDetachedSignatureDataAsync(protectedData, deatachedSignatureData, publicKeyData).ConfigureAwait(false);
+                }
+                                
+                using (var unprotectedData = new MemoryStream())
+                {
+                    protectedData.Position = 0;
+                    deatachedSignatureData.Position = 0;
+                    var signed = await BackupDataSignatureVerifier.VerifySignatureAsync(protectedData, deatachedSignatureData).ConfigureAwait(false);
+
+                    Assert.IsTrue(signed);
+
+                    protectedData.Position = 0;
+                    await BackupDataProtector.UnlockDataAsync(protectedData, unprotectedData).ConfigureAwait(false);                   
+
+                    Assert.AreEqual(TestData.DataToProtect, unprotectedData.ToArray());
+                }
+            }
+        }
+
+        [Test]        
+        [Category("Backup")]
+        public async Task BuildThenParseBackupAsync()
+        {
+            using (var backup = await BuildBackupAsync().ConfigureAwait(true))
+            {
+                await ParseBackupAsync(backup).ConfigureAwait(true);
+            }            
+        }
+
+
+        [Test]        
+        [Category("Backup")]
+        public async Task BackupParseIfNoObjectAsync()
+        {
+            using (var backup = new MemoryStream())
+            {
+                IBackupBuilder builder = BackupSerializationFactory.CreateBackupBuilder();
+
+                await builder.SetVersionAsync(TestData.ProtocolVersion).ConfigureAwait(true);
+
+                await builder.BuildBackupAsync(backup).ConfigureAwait(true);
+
+                backup.Position = 0;
+
+                IBackupParser parser = BackupSerializationFactory.CreateBackupParser();
+
+                await parser.ParseBackupAsync(backup).ConfigureAwait(true);
+
+                var version = await parser.GetVersionAsync().ConfigureAwait(true);
+                Assert.AreEqual(TestData.ProtocolVersion, version);
+
+                Assert.ThrowsAsync<BackupDeserializationException>(() => parser.GetAccountsAsync());
+            }
+        }
+
+        [Test]
+        [Category("Backup")]
+        public async Task BackupImportedPublicKeys()
+        {
+            using (var backup = new MemoryStream())
+            {
+                IBackupBuilder builder = BackupSerializationFactory.CreateBackupBuilder();
+
+                await builder.SetImportedPublicKeysAsync(TestData.ImportedPublicKeys).ConfigureAwait(true);
+
+                await builder.BuildBackupAsync(backup).ConfigureAwait(true);
+
+                backup.Position = 0;
+
+                IBackupParser parser = BackupSerializationFactory.CreateBackupParser();
+
+                await parser.ParseBackupAsync(backup).ConfigureAwait(true);
+
+                var importedPublicKeys = await parser.GetImportedPublicKeysAsync().ConfigureAwait(true);
+
+                Assert.AreEqual(importedPublicKeys, TestData.ImportedPublicKeys);
+            }
+        }
+
+        [Test]        
+        [Category("Backup")]
+        public async Task ParseUnsupportedBackupAsync()
+        {
+            var brokenBackupData = new byte[] { 8, 5, 2, 0 };
+
+            await Task.Run(() =>
+            {
+                using var backupData = new MemoryStream(brokenBackupData);
+                IBackupParser parser = BackupSerializationFactory.CreateBackupParser();
+
+                Assert.ThrowsAsync<BackupParsingException>(() => parser.ParseBackupAsync(backupData));
+            }).ConfigureAwait(true);
+        }
+
+        [Test]
+        [Category("Backup")]
+        public async Task BackupSettings()
+        {
+            using (var backup = new MemoryStream())
+            {
+                IBackupBuilder builder = BackupSerializationFactory.CreateBackupBuilder();
+
+                await builder.SetSettingsAsync(TestData.SomeSettings).ConfigureAwait(true);
+                await builder.BuildBackupAsync(backup).ConfigureAwait(true);
+
+                backup.Position = 0;
+
+                IBackupParser parser = BackupSerializationFactory.CreateBackupParser();
+
+                await parser.ParseBackupAsync(backup).ConfigureAwait(true);
+
+                var settings = await parser.GetSettingsAsync().ConfigureAwait(true);
+
+                Assert.AreEqual(settings.DecentralizedAccountCounter, TestData.SomeSettings.DecentralizedAccountCounter);
+            }
+        }
+    }
+}
---
+++ Sources/Tests/Tuvi.Core.Backup.Impl.Tests/UploadAndDownloadBackupTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Tuvi.Core.Web.BackupService;
+using BackupServiceClientLibrary;
+using NUnit.Framework;
+using Org.BouncyCastle.Bcpg.OpenPgp;
+using SecurityManagementTests;
+using Tuvi.Core.Backup.Impl;
+using TuviPgpLibImpl;
+
+namespace BackupTests
+{
+    public class UploadAndDownloadBackupTests : BaseBackupTest
+    {
+        const string UploadUrl = "https://testnet.eppie.io/api/UploadBackupFunction?code=1";
+        const string DownloadUrl = "https://testnet.eppie.io/api/DownloadBackupFunction?code=1&name=";
+
+        //const string UploadUrl = "http://localhost:7071/api/UploadBackupFunction";
+        //const string DownloadUrl = "http://localhost:7071/api/DownloadBackupFunction?name=";
+
+        [OneTimeSetUp]
+        protected void InitializeContext()
+        {
+            Initialize();
+        }
+
+        [Test]
+        [Category("Upload")]
+        [Category("Download")]
+        [Category("Backup")]
+        public async Task UploadAndDownloadBackupFileAsync()
+        {
+            var publickeyName = Fingerprint + DataIdentificators.PublicKeyExtension;
+            var signatureName = Fingerprint + DataIdentificators.SignatureExtension;
+            var backupName = Fingerprint + DataIdentificators.BackupExtension;
+
+            {// Upload
+
+                using (var publicKeyStream = new MemoryStream())
+                using (var deatachedSignatureData = new MemoryStream())
+                using (var backup = await BuildBackupAsync().ConfigureAwait(true))
+                {
+                    await BackupDataProtector.CreateDetachedSignatureDataAsync(backup, deatachedSignatureData, publicKeyStream).ConfigureAwait(false);
+
+                    var responce = await BackupServiceClient.UploadAsync(new Uri(UploadUrl), Fingerprint, publicKeyStream, deatachedSignatureData, backup).ConfigureAwait(true);
+                    Assert.IsTrue(responce);
+                }
+            }
+
+            {// Download
+
+                using (var backup = await BackupServiceClient.DownloadAsync(new Uri(DownloadUrl), backupName).ConfigureAwait(true))
+                {
+                    var verificationKeyStorage = new MockPgpKeyStorage().Get();
+
+                    using (var publicKeyStream = await BackupServiceClient.DownloadAsync(new Uri(DownloadUrl), publickeyName).ConfigureAwait(true))
+                    using (var verificationContext = new TuviPgpContext(verificationKeyStorage))
+                    {
+                        await verificationContext.LoadContextAsync().ConfigureAwait(true);
+
+                        var ring = new PgpPublicKeyRing(publicKeyStream);
+                        var bundle = new PgpPublicKeyRingBundle(new PgpObject[] { ring });
+                        verificationContext.Import(bundle);
+
+                        var backupDataSignatureVerifier = BackupProtectorCreator.CreateBackupProtector(verificationContext);
+                        backupDataSignatureVerifier.SetPgpKeyIdentity(TestData.BackupPgpKeyIdentity);
+
+                        using (var deatachedSignatureData = await BackupServiceClient.DownloadAsync(new Uri(DownloadUrl), signatureName).ConfigureAwait(true))
+                        {
+                            var signed = await backupDataSignatureVerifier.VerifySignatureAsync(backup, deatachedSignatureData).ConfigureAwait(false);
+                            Assert.IsTrue(signed);
+                        }
+                    }
+
+                    backup.Position = 0;
+                    await ParseBackupAsync(backup).ConfigureAwait(true);
+                }
+            }
+        }
+    }
+}
