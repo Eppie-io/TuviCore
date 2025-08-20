60660409e732a8b8f02e3275185896cd9cda7c55
Valera <valera@finebits.com>
2025-08-20 15:37:35 +0300

    refactor: change dec storage
    (cherry picked from commit 137de5e0f5c1db4545ce3c1af1a32b7dd8b04339)

--- Sources/Tuvi.Core.Dec.Web.Impl/WebDecStorageClient.cs
+++ Sources/Tuvi.Core.Dec.Web.Impl/WebDecStorageClient.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -29,29 +30,34 @@ namespace Tuvi.Core.Dec.Web.Impl
             Url = url;
         }
 
-        public async Task<string> SendAsync(string address, byte[] data)
+        public async Task<string> SendAsync(string address, string hash)
+        {
+            return await Client.GetStringAsync($"{Url}/send?address={address}&hash={hash}&code=testnet").ConfigureAwait(false);
+        }
+
+        public async Task<IEnumerable<string>> ListAsync(string address)
+        {
+            var list = await Client.GetStringAsync($"{Url}/list?address={address}&code=testnet").ConfigureAwait(false);
+            return JsonSerializer.Deserialize<IEnumerable<string>>(list);
+        }
+
+        public async Task<byte[]> GetAsync(string hash)
+        {
+            return await Client.GetByteArrayAsync($"{Url}/get?hash={hash}&code=testnet").ConfigureAwait(false);
+        }
+
+        public async Task<string> PutAsync(byte[] data)
         {
             using (var dataStream = new MemoryStream(data))
             using (var dataContent = new StreamContent(dataStream))
             using (var formData = new MultipartFormDataContent())
             {
-                formData.Add(dataContent, address, address);
+                formData.Add(dataContent, "data", "data");
 
-                var response = await Client.PostAsync($"{Url}/send?folder={address}&code=85sEA3CukAJ8CUg0fbMgH3KoNB-gWhj9iDnMvDvTB384AzFuIuPw6w==", formData).ConfigureAwait(false);
+                var response = await Client.PostAsync($"{Url}/put?code=testnet", formData).ConfigureAwait(false);
 
                 return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
             }
-        }
-
-        public async Task<IEnumerable<string>> ListAsync(string address)
-        {
-            var list = await Client.GetStringAsync($"{Url}/list?folder={address}&code=x3XfWh-wwuxHxvoA1S0bAwo6Zocq4DrccaiECMW-nZwGAzFu0lhI6Q==").ConfigureAwait(false);
-            return JsonSerializer.Deserialize<IEnumerable<string>>(list);
-        }
-
-        public async Task<byte[]> GetAsync(string address, string hash)
-        {
-            return await Client.GetByteArrayAsync($"{Url}/get?folder={address}&hash={hash}&code=nyzDT27rYdP4x2Dx97CoPzBbfBsRQ8cti3oJ092R2BCGAzFuzMm1dg==").ConfigureAwait(false);
         }
 
         protected virtual void Dispose(bool disposing)
--- Sources/Tuvi.Core.Dec/DecStorageClient.cs
+++ Sources/Tuvi.Core.Dec/DecStorageClient.cs
@@ -6,10 +6,12 @@ namespace Tuvi.Core.Dec
 {
     public interface IDecStorageClient : IDisposable
     {
-        Task<string> SendAsync(string address, byte[] data);
+        Task<string> SendAsync(string address, string hash);
 
         Task<IEnumerable<string>> ListAsync(string address);
 
-        Task<byte[]> GetAsync(string address, string hash);
+        Task<byte[]> GetAsync(string hash);
+
+        Task<string> PutAsync(byte[] data);
     }
 }
