3090dfdaa9195927114a6a1a28b8915fca28ede9
Valera <valera@finebits.com>
2025-08-21 13:02:32 +0300

    fix: fix warnings

--- Sources/Tuvi.Core.Impl/AccountService.cs
+++ Sources/Tuvi.Core.Impl/AccountService.cs
@@ -1,4 +1,22 @@
-﻿using Microsoft.Extensions.Logging;
+﻿// ---------------------------------------------------------------------------- //
+//                                                                              //
+//   Copyright 2025 Eppie (https://eppie.io)                                    //
+//                                                                              //
+//   Licensed under the Apache License, Version 2.0 (the "License"),            //
+//   you may not use this file except in compliance with the License.           //
+//   You may obtain a copy of the License at                                    //
+//                                                                              //
+//       http://www.apache.org/licenses/LICENSE-2.0                             //
+//                                                                              //
+//   Unless required by applicable law or agreed to in writing, software        //
+//   distributed under the License is distributed on an "AS IS" BASIS,          //
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   //
+//   See the License for the specific language governing permissions and        //
+//   limitations under the License.                                             //
+//                                                                              //
+// ---------------------------------------------------------------------------- //
+
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -224,7 +242,7 @@ namespace Tuvi.Core.Impl
         private async Task<Message> GetMessageBodyImplAsync(Message message, Message fullMessage, CancellationToken cancellationToken = default)
         {
             message = await UpdateMessageAsync(message, fullMessage, cancellationToken).ConfigureAwait(false);
-            
+
             await MessageProtector.TryVerifyAndDecryptAsync(message, cancellationToken).ConfigureAwait(false);
             await DataStorage.UpdateMessageAsync(Account.Email, message, updateUnreadAndTotal: true, cancellationToken).ConfigureAwait(false);
 
