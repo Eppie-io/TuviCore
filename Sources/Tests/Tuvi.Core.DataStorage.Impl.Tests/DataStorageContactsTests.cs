5b884b6a97056f66d17c711d841fb9602541fca0
Valera <50830352+ValeraFinebits@users.noreply.github.com>
2025-12-03 14:38:33 +0200

    feat: add external content policy to Account (#326)
    * feat: add external content policy to Account
    
    - Introduce `ExternalContentPolicy` enum to manage external content.
    - Add `ExternalContentPolicy` property to `Account` with default value.
    - Update `Account.Default` to initialize the new property.
    - Enhance `Equals` method to compare the new property.
    - Add copyright and license header to the file.
    
    * fix: add tests

--- Sources/Tests/Tuvi.Core.DataStorage.Impl.Tests/DataStorageContactsTests.cs
+++ Sources/Tests/Tuvi.Core.DataStorage.Impl.Tests/DataStorageContactsTests.cs
@@ -1,4 +1,22 @@
-﻿using System.Collections.Generic;
+﻿// ---------------------------------------------------------------------------- //
+//                                                                              //
+//   Copyright 2025 Eppie (https://eppie.io)                                    //
+//                                                                              //
+//   Licensed under the Apache License, Version 2.0 (the "License"),            //
+//   you may not use this file except in compliance with the License.           //
+//   You may obtain a copy of the License at                                    //
+//                                                                              //
+//       http://www.apache.org/licenses/LICENSE-2.0                             //
+//                                                                              //
+//   Unless required by applicable law or agreed to in writing, software        //
+//   distributed under the License is distributed on an "AS IS" BASIS,          //
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   //
+//   See the License for the specific language governing permissions and        //
+//   limitations under the License.                                             //
+//                                                                              //
+// ---------------------------------------------------------------------------- //
+
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -86,7 +104,7 @@ namespace Tuvi.Core.DataStorage.Tests
             if (i1.Width == i2.Width && i1.Height == i2.Height)
             {
                 if ((i1.Bytes == null && i2.Bytes == null) ||
-                    i1.Bytes != null && i2.Bytes != null && i1.Bytes.SequenceEqual(i2.Bytes))
+                    (i1.Bytes != null && i2.Bytes != null && i1.Bytes.SequenceEqual(i2.Bytes)))
                 {
                     return true;
                 }
---
+++ Sources/Tests/Tuvi.Core.Entities.Tests/AccountTests.cs
@@ -0,0 +1,132 @@
+﻿// ---------------------------------------------------------------------------- //
+//                                                                              //
+//   Copyright 2025 Eppie (https://eppie.io)                                    //
+//                                                                              //
+//   Licensed under the Apache License, Version 2.0 (the "License"),            //
+//   you may not use this file except in compliance with the License.           //
+//   You may obtain a copy of the License at                                    //
+//                                                                              //
+//       http://www.apache.org/licenses/LICENSE-2.0                             //
+//                                                                              //
+//   Unless required by applicable law or agreed to in writing, software        //
+//   distributed under the License is distributed on an "AS IS" BASIS,          //
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   //
+//   See the License for the specific language governing permissions and        //
+//   limitations under the License.                                             //
+//                                                                              //
+// ---------------------------------------------------------------------------- //
+
+using NUnit.Framework;
+
+namespace Tuvi.Core.Entities.Test
+{
+    public class AccountTests
+    {
+        [Test]
+        public void AccountDefaultHasCorrectExternalContentPolicy()
+        {
+            var account = Account.Default;
+
+            Assert.That(account.ExternalContentPolicy, Is.EqualTo(ExternalContentPolicy.AlwaysAllow));
+        }
+
+        [Test]
+        public void AccountEqualityWithSameExternalContentPolicy()
+        {
+            var account1 = new Account
+            {
+                Id = 1,
+                Email = new EmailAddress("test@test.com"),
+                ExternalContentPolicy = ExternalContentPolicy.Block,
+                IncomingServerAddress = "imap.test.com",
+                IncomingServerPort = 993,
+                IncomingMailProtocol = MailProtocol.IMAP,
+                OutgoingServerAddress = "smtp.test.com",
+                OutgoingServerPort = 465,
+                OutgoingMailProtocol = MailProtocol.SMTP,
+                AuthData = new BasicAuthData { Password = "test" }
+            };
+
+            var account2 = new Account
+            {
+                Id = 1,
+                Email = new EmailAddress("test@test.com"),
+                ExternalContentPolicy = ExternalContentPolicy.Block,
+                IncomingServerAddress = "imap.test.com",
+                IncomingServerPort = 993,
+                IncomingMailProtocol = MailProtocol.IMAP,
+                OutgoingServerAddress = "smtp.test.com",
+                OutgoingServerPort = 465,
+                OutgoingMailProtocol = MailProtocol.SMTP,
+                AuthData = new BasicAuthData { Password = "test" }
+            };
+
+            Assert.That(account1, Is.EqualTo(account2));
+        }
+
+        [Test]
+        public void AccountEqualityWithDifferentExternalContentPolicy()
+        {
+            var account1 = new Account
+            {
+                Id = 1,
+                Email = new EmailAddress("test@test.com"),
+                ExternalContentPolicy = ExternalContentPolicy.AlwaysAllow,
+                IncomingServerAddress = "imap.test.com",
+                IncomingServerPort = 993,
+                IncomingMailProtocol = MailProtocol.IMAP,
+                OutgoingServerAddress = "smtp.test.com",
+                OutgoingServerPort = 465,
+                OutgoingMailProtocol = MailProtocol.SMTP,
+                AuthData = new BasicAuthData { Password = "test" }
+            };
+
+            var account2 = new Account
+            {
+                Id = 1,
+                Email = new EmailAddress("test@test.com"),
+                ExternalContentPolicy = ExternalContentPolicy.Block,
+                IncomingServerAddress = "imap.test.com",
+                IncomingServerPort = 993,
+                IncomingMailProtocol = MailProtocol.IMAP,
+                OutgoingServerAddress = "smtp.test.com",
+                OutgoingServerPort = 465,
+                OutgoingMailProtocol = MailProtocol.SMTP,
+                AuthData = new BasicAuthData { Password = "test" }
+            };
+
+            Assert.That(account1, Is.Not.EqualTo(account2));
+        }
+
+        [Test]
+        public void ExternalContentPolicyEnumValues()
+        {
+            Assert.That((int)ExternalContentPolicy.AlwaysAllow, Is.EqualTo(0));
+            Assert.That((int)ExternalContentPolicy.AskEachTime, Is.EqualTo(1));
+            Assert.That((int)ExternalContentPolicy.Block, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void AccountConstructorSetsDefaultExternalContentPolicy()
+        {
+            var account = new Account();
+
+            Assert.That(account.ExternalContentPolicy, Is.EqualTo(ExternalContentPolicy.AlwaysAllow));
+        }
+
+        [Test]
+        public void AccountCanSetExternalContentPolicy()
+        {
+            var account = new Account
+            {
+                ExternalContentPolicy = ExternalContentPolicy.AskEachTime
+            };
+
+            Assert.That(account.ExternalContentPolicy, Is.EqualTo(ExternalContentPolicy.AskEachTime));
+
+            account.ExternalContentPolicy = ExternalContentPolicy.Block;
+
+            Assert.That(account.ExternalContentPolicy, Is.EqualTo(ExternalContentPolicy.Block));
+        }
+    }
+}
