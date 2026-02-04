268bd04c955a49ad1102af40c73788c50bc7f72d
Valera <50830352+ValeraFinebits@users.noreply.github.com>
2026-02-04 14:12:37 +0200

    Remove DeleteFolderAsync method for folder deletion
    Removed unsupported DeleteFolderAsync method.

--- Sources/Tuvi.Proton.Impl/ProtonMailBox.cs
+++ Sources/Tuvi.Proton.Impl/ProtonMailBox.cs
@@ -1136,11 +1136,6 @@ namespace Tuvi.Proton
             throw new NotSupportedException("Renaming folders in Proton Mail is not supported.");
         }
 
-        public Task DeleteFolderAsync(Folder folder, CancellationToken cancellationToken = default)
-        {
-            throw new NotSupportedException("Proton Mail does not support folder deletion.");
-        }
-
         #endregion
     }
 }
